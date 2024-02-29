ee27b39865643e793e1e6b3e32ca83e47b9c8d94
kunrak <rakeshkunducsgi@gmail.com>
2024-02-29 12:41:15 +0530

    completed UserList.test.js

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
+function renderComponent() {
   const users = [
     { name: "jane", email: "jane@gmail.com" },
     { name: "sam", email: "sam@gmail.com" },
@@ -9,7 +9,28 @@ test("render one row per user", () => {
 
   render(<UserList users={users} />);
 
-  const rows = screen.getAllByRole("row");
+  return {
+    users,
+  };
+}
+
+test("render one row per user", () => {
+  renderComponent();
+
+  //eslint-disable-next-line
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   expect(rows).toHaveLength(2);
+});
+
+test("render the name and email of each user", () => {
+  const { users } = renderComponent();
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
 });
